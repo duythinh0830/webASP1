67d39e54c51ff845c4eb9fe3ca1de97c70dba83e
duythinh0830 <doanduythinh0830@gmail.com>
2025-03-21 23:23:54 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NewsWebsite.Models;
+using NewsWebsite.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<NewsDbContext>(options =>
+    options.UseSqlite("Data Source=news.db"));
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
